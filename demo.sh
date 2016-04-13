fbd8814d1c217ff2fa0946949de13a096222073e
Jack Cherng <jfcherng@gmail.com>
2016-04-13 20:14:39 +0800

    Update demo shellscript

--- demo.sh
+++ demo.sh
@@ -3,31 +3,35 @@
 # This file is a sort of test suite for bash syntax highlighting. It is not
 # intended to be parsed or run, just viewed in an editor.
 
-# ################################################
-# Strings and interpolation in parameter expansion
-# ################################################
+
+####################################################
+# Strings and interpolation in parameter expansion #
+####################################################
+
 # Strings appearing in expansion constructs should be treated as such.
-: ${foo:-bar}     # 'bar' is a literal string
-: ${foo:='bar'}   # "'bar'" is a literal string
-: ${foo//bar/baz} # 'bar' and 'baz' are literal strings
+${foo:-bar}     # 'bar' is a literal string
+${foo:='bar'}   # "'bar'" is a literal string
+${foo//bar/baz} # 'bar' and 'baz' are literal strings
 
 # Strings in expansion constructs should support interpolation.
-: ${foo:=`bar`}        # Command substitution should be recognized
-: ${foo:=$( bar )}     # Command substitution should be recognized
-: ${foo:=$(( 1 + 1 ))} # Arithmetic substitution should be recognized
-: ${foo:=$bar}         # Variables should be recognized
-: ${foo:="$bar"}       # Variables should be recognized
+${foo:=`bar`}        # Command substitution should be recognized
+${foo:=$( bar )}     # Command substitution should be recognized
+${foo:=$(( 1 + 1 ))} # Arithmetic substitution should be recognized
+${foo:=$bar}         # Variables should be recognized
+${foo:="$bar"}       # Variables should be recognized
 
-# #############################
-# Braces in parameter expansion
-# #############################
+
+#################################
+# Braces in parameter expansion #
+#################################
+
 # The '\}' on the following line should be treated as a string literal (or
 # escape sequence); it should NOT terminate the expansion construct.
-: ${foo//foo\}foo\/foo/foo}
+${foo//foo\}foo\/foo/foo}
 # The quoted part of the following line should be treated as a string, with the
 # '${bar}' part specifically being an interpolated variable. The closing '}' in
 # '${bar}' should NOT terminate the '${foo:=...}' expansion construct.
-: ${foo:="${bar} baz"}
+${foo:="${bar} baz"}
 
 # Sublime bug fix: "
 
@@ -41,58 +45,61 @@ ${var%Pattern}
 ${var%%Pattern}
 ${var%%%Pattern}
 
-##################################################################
-# Parameter-expansion operators
-# cf. http://www.tldp.org/LDP/abs/html/parameter-substitution.html
-##################################################################
+
+####################################################################
+# Parameter-expansion operators                                    #
+# cf. http://www.tldp.org/LDP/abs/html/parameter-substitution.html #
+####################################################################
+
 # Parameter-expansion operators should be treated as such only where
 # appropriate. They should NOT be treated as unqualified operators anywhere they
 # appear in an expansion construct.
-: ${foo//%/} # '%' is not an operator here
-: ${foo//#/} # '#' is not an operator here
-: ${foo//!/} # '!' is not an operator here
-: ${foo//:/} # ':' is not an operator here
-: ${foo//@/} # '@' is not an operator here
+${foo//%/} # '%' is not an operator here
+${foo//#/} # '#' is not an operator here
+${foo//!/} # '!' is not an operator here
+${foo//:/} # ':' is not an operator here
+${foo//@/} # '@' is not an operator here
 
 # Expansion operators which can only appear before an identifier, such as '#'
 # and '!', should not be treated as expansion operators after an identifier.
-: ${foo#} # duplicated with ${var#Pattern} where 'Pattern' is empty?
-: ${foo!} # '!' is not a operator here indeed
-: ${var:pos:len}
-: ${var:pos:len}
+${foo#} # duplicated with ${var#Pattern} where 'Pattern' is empty?
+${foo!} # '!' is not a operator here indeed
+${var:pos:len}
 
 # When using the replace operators ('/' and '//'), escaped and un-escaped
 # slashes in the needle and replacement strings should be handled correctly.
 # Slashes need be escaped only in the needle; escaping slashes in the
 # replacement is optional.
-: ${foo//a\/b/c/d}
+${foo//a\/b/c/d}
 #      ^^    ^     These slashes are operators
 #          ^   ^   These are not
 
 # The '^' and ',' expansion operators (and friends) should be treated as such,
 # where appropriate.
-: ${foo^}  # '^'  is an operator (upper-case initial)
-: ${foo,}  # ','  is an operator (lower-case initial)
-: ${foo^^} # '^^' is an operator (upper-case all)
-: ${foo,,} # ',,' is an operator (lower-case all)
+${foo^}  # '^'  is an operator (upper-case initial)
+${foo,}  # ','  is an operator (lower-case initial)
+${foo^^} # '^^' is an operator (upper-case all)
+${foo,,} # ',,' is an operator (lower-case all)
 
-: ${foo,,,Pattern}
-: ${foo,,Pattern}
+${foo,,,Pattern}
+${foo,,Pattern}
 
 # The '-', '+', '=', and '?' expansion operators (and friends) should be treated
 # as such, where appropriate.
-: ${foo-bar}  # '-'  is an operator (substitute if unset)
-: ${foo:-bar} # ':-' is an operator (substitute if unset or null)
-: ${foo+bar}  # '+'  is an operator (substitute if set)
-: ${foo:+bar} # ':+' is an operator (substitute if set and not null)
-: ${foo=bar}  # '='  is an operator (set if not set)
-: ${foo:=bar} # ':=' is an operator (set if not set or null)
-: ${foo?bar}  # '='  is an operator (abort if not set)
-: ${foo:?bar} # ':=' is an operator (abort if not set or null)
+${foo-bar}  # '-'  is an operator (substitute if unset)
+${foo:-bar} # ':-' is an operator (substitute if unset or null)
+${foo+bar}  # '+'  is an operator (substitute if set)
+${foo:+bar} # ':+' is an operator (substitute if set and not null)
+${foo=bar}  # '='  is an operator (set if not set)
+${foo:=bar} # ':=' is an operator (set if not set or null)
+${foo?bar}  # '='  is an operator (abort if not set)
+${foo:?bar} # ':=' is an operator (abort if not set or null)
 
-#################
-# Misc. operators
-#################
+
+###################
+# Misc. operators #
+###################
+
 # The '=' in a variable assignment should be recognized as an assignment
 # operator.
 foo='bar'
@@ -114,13 +121,12 @@ echo [[ "${foo}" != 'bar' ]]
 
 # Semi-colons should be highlighted as operators in C-style loop constructs.
 foo; bar; baz
-for (( i = 0; i < RAND; i++ )); do
-    :
-done
 
-##################
-# Identifier names
-##################
+
+####################
+# Identifier names #
+####################
+
 # The left side of a variable assignment should be recognized as a variable
 # name.
 foo='bar'
@@ -152,16 +158,18 @@ read -a foo         # 'foo' is a variable name
 
 # Variable names in arithmetic and C-style loop constructs should be treated as
 # such.
-: $(( foo + 2 )) # 'foo' is a variable name
+$(( foo + 2 )) # 'foo' is a variable name
 (( foo++ ))      # 'foo' is a variable name
 
 for (( i = 0; i < RAND; i++ )); do # 'i' and 'RAND' are variable names
     :
 done
 
-##############################################
-# Quoting, interpolation and nested constructs
-##############################################
+
+################################################
+# Quoting, interpolation and nested constructs #
+################################################
+
 # Regular-expression patterns should be highlighted as regular expressions, or
 # at least strings.
 [[ "${foo}" =~ ^(bar|baz)$ ]]
@@ -186,14 +194,16 @@ EOF
 
 # Redirection can be used right after heredoc
 cat << EOF > /tmp/yourfilehere
-$variable
+    $variable
 These contents will be written to the file.
         This line is indented.
 EOF
 
-##############################
-# Heredoc with compact-command
-##############################
+
+################################
+# Heredoc with compact-command #
+################################
+
 print_info_text_compact () { cat <<EOF; do_something; }
 This is some info text.
 EOF
@@ -206,9 +216,11 @@ EOF
 
 # scope before this line should be `source.shell`
 
-######################
-# Nomal Heredoc usages
-######################
+
+########################
+# Nomal Heredoc usages #
+########################
+
 print_info_text () { cat <<EOF
 This is some info text.
 EOF
@@ -231,9 +243,11 @@ Condition is met.
 EOF
 fi
 
-#############
-# Redirection
-#############
+
+###############
+# Redirection #
+###############
+
 # Redirect stdout to file "filename."
 1>filename
 # Redirect and append stdout to file "filename."
@@ -247,36 +261,61 @@ fi
 0<&-
 <&-
 
-##########
-# Subshell
-##########
+
+############
+# Subshell #
+############
+
 # Substitution constructs inside double-quotes should be recognized as such.
-: "$( foo )"     # $( foo ) should be interpolated
-: "`  foo `"     # `  foo ` should be interpolated
-: "$(( foo++ ))" # $(( foo++ )) should be interpolated
+"$( foo )"     # $( foo ) should be interpolated
+"`  foo `"     # `  foo ` should be interpolated
+"$(( foo++ ))" # $(( foo++ )) should be interpolated
 
 # Substitution constructs should support nesting.
-: $( foo $( bar $( baz ) ) )
-: $( foo ` bar ` )
-: $( foo "$(( bar + 1 ))" )
+$( foo $( bar $( baz ) ) )
+$( foo ` bar ` )
+$( foo "$(( bar + 1 ))" )
 foo="$( bar "$( baz "$( qux )" )" )"
 $(( ( RANDOM * 100 ) / 5 ))
 $(( a=(2*(250+1))/5 ))
 
-###############
-# controversial
-###############
+
+#################
+# controversial #
+#################
+
 empty=$((echo hello) > output.txt)
 # Sublime bug fix: ))
 empty=$( (echo hello) > output.txt)
 
 grep 'foo' <( bar "$( baz )" )
 
-#########################
-# Misc. language features
-#########################
+
+####################
+# Command Switches #
+####################
+
+echo -e Hello
+gcc input.c -o output.exe --verbose
+
+
+##################
+# Special Design #
+##################
+
+# following "true" are scoped "variable.other.true.shell"
+var=true
+# following "false" are scoped "variable.other.false.shell"
+var=false
+
+
+###########################
+# Misc. language features #
+###########################
+
 # The `in` in a `case` statement should be highlighted as a control key word.
 case "${foo}" in
+    ( help | -h ) bar ;;
     *) bar ;;
 esac
 
@@ -305,11 +344,8 @@ cat=1                       # 'cat' is not a executable here
 
 # Test cases for 'echo'. Things following 'echo' may be considered as strings.
 echo git rev-list "$(echo --all)" | grep -P 'c354a80'
-echo git rev-list "$(echo --all)"
 echo $(echo git --version) echo `$(echo git --version)` "$(echo git --version)"
 echo `echo git --version`
-echo echo $(echo git --version)
-echo echo $(echo echo Hello) | grep -P 'c354a80'
 echo echo $(git) "$(git)" `$(git)` `git` | grep -P 'c354a80'
 
 echo cat \
